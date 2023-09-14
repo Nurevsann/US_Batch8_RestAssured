c619aa012cef6bd0d2b5d33b7ee178fe0e73bc77
ragipbaylar <ragipbaylar86@gmail.com>
2023-09-14 14:06:49 -0400

    Day 05 - first commit

--- src/test/java/GoRestUsersTest.java
+++ src/test/java/GoRestUsersTest.java
@@ -1,5 +1,11 @@
 import POJOClasses.User;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,26 +27,36 @@ public class GoRestUsersTest {
         return RandomStringUtils.randomAlphabetic(10) + "@techno.com";
     }
 
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+
     @BeforeClass
     public void setUp() {
 
         baseURI = "https://gorest.co.in/public/v2/users";
 
+        requestSpecification = new RequestSpecBuilder()
+                .addHeader("Authorization", "Bearer 07a0637daa1168c03f668358e917ecafd9ee10fbc7b0df4899707e3476af87fe")
+                .setContentType(ContentType.JSON)
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .log(LogDetail.BODY)
+                .expectContentType(ContentType.JSON)
+                .build();
     }
 
 
     @Test
     void createNewUser() {
         given()
-                .header("Authorization", "Bearer 07a0637daa1168c03f668358e917ecafd9ee10fbc7b0df4899707e3476af87fe")
-                .contentType(ContentType.JSON)
+                .spec(requestSpecification)
                 .body("{\"name\":\"" + createRandomName() + "\",\"gender\":\"male\",\"email\":\"" + createRandomEmail() + "\",\"status\":\"active\"}")
                 .when()
                 .post("")
                 .then()
-                .log().body()
                 .statusCode(201)
-                .contentType(ContentType.JSON);
+                .spec(responseSpecification);
     }
 
     @Test
@@ -52,38 +68,78 @@ public class GoRestUsersTest {
         user.put("status", "active");
 
         given()
-                .header("Authorization", "Bearer 07a0637daa1168c03f668358e917ecafd9ee10fbc7b0df4899707e3476af87fe")
-                .contentType(ContentType.JSON)
+                .spec(requestSpecification)
                 .body(user)
                 .when()
                 .post("")
                 .then()
-                .log().body()
                 .statusCode(201)
-                .contentType(ContentType.JSON)
+                .spec(responseSpecification)
                 .body("email", equalTo(user.get("email")));
     }
 
+    User user;
+    Response response;
+
     @Test
     void createNewUserWithObject() {
-        User user = new User();
+        user = new User();
         user.setName(createRandomName());
         user.setEmail(createRandomEmail());
         user.setGender("female");
         user.setStatus("active");
 
-        given()
-                .header("Authorization", "Bearer 07a0637daa1168c03f668358e917ecafd9ee10fbc7b0df4899707e3476af87fe")
-                .contentType(ContentType.JSON)
+        response = given()
+                .spec(requestSpecification)
                 .body(user)
                 .when()
                 .post("")
                 .then()
-                .log().body()
                 .statusCode(201)
-                .contentType(ContentType.JSON)
-                .body("email", equalTo(user.getEmail()));
+                .spec(responseSpecification)
+                .body("email", equalTo(user.getEmail()))
+                .extract().response();
     }
     //TODO
-    /** Write create user negative test**/
+
+    /**
+     * Write create user negative test
+     **/
+
+    @Test(dependsOnMethods = "createNewUserWithObject")
+    void createUserNegativeTest() {
+        user.setName(createRandomName());
+        user.setGender("male");
+
+        given()
+                .spec(requestSpecification)
+                .body(user)
+                .when()
+                .post("")
+                .then()
+                .statusCode(422)
+                .spec(responseSpecification)
+                .body("[0].message", equalTo("has already been taken"));
+        // [0].message -> body[0].message
+    }
+
+    /**
+     * get the user you created in createAUserWithObjects test
+     **/
+
+    @Test(dependsOnMethods = "createNewUserWithObject")
+    void getUserById() {
+        given()
+                .spec(requestSpecification)
+                .pathParam("userId", response.path("id"))
+                .when()
+                .get("/{userId}")
+                .then()
+                .statusCode(200)
+                .spec(responseSpecification)
+                .body("name", equalTo(user.getName()))
+                .body("email", equalTo(user.getEmail()));
+    }
+
+
 }
