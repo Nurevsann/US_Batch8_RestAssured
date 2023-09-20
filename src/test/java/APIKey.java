15c62b02d3067d771cd5301ce2e250d18c5a32f6
nurev <nurevsankurak@gmail.com>
2023-09-20 20:40:32 +0300

    edit

--- src/test/java/APIKey.java
+++ src/test/java/APIKey.java
@@ -36,7 +36,6 @@ public class APIKey {
                 .when()
                 .get("http://api.weatherapi.com/v1/current.json")
                 .then()
-                .statusCode(405)
                 //.log().body()
                 .extract().response();
 
